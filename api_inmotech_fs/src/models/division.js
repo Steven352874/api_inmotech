7667751bf23782b7de50fb3206f553362cf91047
Steven352874 <STEVEN.19972015@OUTLOOK.COM>
2025-04-06 11:46:54 -0500

    Create division.js

---
+++ api_inmotech_fs/src/models/division.js
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../database/index.js';
+
+const division = sequelize.define('division', {
+    Division_id: {
+        type: DataTypes.INTEGER(11),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    Division_nombre: {
+        type: DataTypes.STRING(255), // Adjust length as needed
+        allowNull: false,
+        collate: 'utf8_general_ci',
+    },
+    Division_descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        collate: 'utf8_general_ci',
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+    },
+    updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+    },
+}, {
+    tableName: 'division',
+});
+
+export default division;
