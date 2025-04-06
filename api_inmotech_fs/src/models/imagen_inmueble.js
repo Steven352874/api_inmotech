56a0ec9f611041d0848545c3afaf4381bfd1277f
Steven352874 <STEVEN.19972015@OUTLOOK.COM>
2025-04-06 10:41:56 -0500

    Update and rename imagen_impuesto.js to imagen_inmueble.js

--- api_inmotech_fs/src/models/imagen_inmueble.js
+++ api_inmotech_fs/src/models/imagen_inmueble.js
@@ -1,24 +1,24 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/index.js';
 
-const imagenImpuesto = sequelize.define('imagen_impuesto', {
-    Imagen_impuesto_id: {
+const imagenInmueble = sequelize.define('imagen_inmueble', {
+    Imagen_inmueble_id: {
         type: DataTypes.INTEGER(11),
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
     },
-    Imagen_impuesto_url: {
+    Imagen_inmueble_url: {
         type: DataTypes.STRING(255), // Adjust length as needed
         allowNull: false,
         collate: 'utf8_general_ci',
     },
-    Imagen_impuesto_descripcion: {
+    Imagen_inmueble_descripcion: {
         type: DataTypes.TEXT,
         allowNull: true,
         collate: 'utf8_general_ci',
     },
-    Impuesto_FK: {
+    Inmueble_FK: {
         type: DataTypes.INTEGER(11),
         allowNull: false,
     },
@@ -31,7 +31,7 @@ const imagenImpuesto = sequelize.define('imagen_impuesto', {
         type: DataTypes.DATE,
     },
 }, {
-    tableName: 'imagen_impuesto',
+    tableName: 'imagen_inmueble',
 });
 
-export default imagenImpuesto;
+export default imagenInmueble;
